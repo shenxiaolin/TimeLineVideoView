817ebfe48f7116148592730fefa4c745b2e659c0
Kirill Stom <stom.k@dbbest.com>
2015-05-15 18:10:10 +0300

    "long" issue fix

--- app/src/main/java/com/stom/videoseekerproject/TimeIndicatorView.java
+++ app/src/main/java/com/stom/videoseekerproject/TimeIndicatorView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
@@ -15,20 +16,19 @@ import android.view.View;
 public class TimeIndicatorView extends View implements View.OnTouchListener {
 
     public static interface OnSeekListener {
-        void onTimeIndicatonSeek(float time);
+        void onTimeIndicatonSeek(long time);
     }
 
     private static final long MAX_DURATION = 30000;
 
-    private static final int BACKGROUND_COLOR = Color.GRAY;
-    private static final int SCALE_COLOR = Color.YELLOW;
-    private static final int INDICATOR_COLOR = Color.BLACK;
-
     public static final int TOTAL_HEIGHT = 50;
     private static final int SCALE_HEIGHT_DP = 5;
     public static final int INDICATOR_WIDTH_DP = 30;
-
     private static final float SCALE_STROKE_WIDTH = 5f;
+
+    private static final int BACKGROUND_COLOR = Color.GRAY;
+    private static final int SCALE_COLOR = Color.YELLOW;
+    private static final int INDICATOR_COLOR = Color.BLACK;
 
     private OnSeekListener listener;
 
@@ -36,8 +36,10 @@ public class TimeIndicatorView extends View implements View.OnTouchListener {
 
     private long startTime = 0;
     private long endTime = 30000;
+
     private float position;
     private float left, right;
+    private int width, height;
 
     private Paint backgroundPaint = new Paint();
     private Paint scalePaint = new Paint();
@@ -49,6 +51,16 @@ public class TimeIndicatorView extends View implements View.OnTouchListener {
 
     public TimeIndicatorView(Context context) {
         super(context);
+        initView();
+    }
+
+    public TimeIndicatorView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    public TimeIndicatorView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
         initView();
     }
 
@@ -87,8 +99,8 @@ public class TimeIndicatorView extends View implements View.OnTouchListener {
         invalidate();
     }
 
-    public float getCurrentTime() {
-        return position / (right - left) * getDuration();
+    public long getCurrentTime() {
+        return (long) (position / (right - left) * getDuration());
     }
 
     public float getDuration() {
@@ -103,33 +115,36 @@ public class TimeIndicatorView extends View implements View.OnTouchListener {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int width = MeasureSpec.getSize(widthMeasureSpec);
         setMeasuredDimension(width, totalHeight);
-
-        invalidateSize();
+        if (width > 0 && totalHeight > 0) {
+            this.width = width;
+            this.height = totalHeight;
+            invalidateSize();
+        }
     }
 
     private void invalidateSize() {
-        left = indicatorWidth / 2 + (getWidth() - indicatorWidth) * startTime / MAX_DURATION;
-        right = indicatorWidth / 2 + (getWidth() - indicatorWidth) * endTime / MAX_DURATION;
+        left = indicatorWidth / 2 + (width - indicatorWidth) * startTime / MAX_DURATION;
+        right = indicatorWidth / 2 + (width - indicatorWidth) * endTime / MAX_DURATION;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawRect(left, 0f, right, getHeight(), backgroundPaint);
+        canvas.drawRect(left, 0f, right, height, backgroundPaint);
         drawScale(canvas);
         drawIndicator(canvas);
     }
 
     private void drawScale(Canvas canvas) {
-        float secondSize = (right - left) / (endTime - startTime);
+        float secondSize = (right - left) / (endTime - startTime) * 1000f;
 
         Path path = new Path();
-        path.moveTo(left, getHeight() - SCALE_STROKE_WIDTH / 2f);
-        path.lineTo(right, getHeight() - SCALE_STROKE_WIDTH / 2f);
+        path.moveTo(left, height - SCALE_STROKE_WIDTH / 2f);
+        path.lineTo(right, height - SCALE_STROKE_WIDTH / 2f);
         for (int i = 1; i * secondSize < right - left; i++) {
-            path.moveTo(left + i * secondSize, getHeight());
-            path.lineTo(left + i * secondSize, getHeight() - (i % 5 == 0 ? scaleHeight * 2 : scaleHeight));
+            path.moveTo(left + i * secondSize, height);
+            path.lineTo(left + i * secondSize, height - (i % 5 == 0 ? scaleHeight * 2 : scaleHeight));
         }
         path.close();
 
@@ -138,10 +153,10 @@ public class TimeIndicatorView extends View implements View.OnTouchListener {
 
     private void drawIndicator(Canvas canvas) {
         Path path = new Path();
-        path.moveTo(left + position, getHeight());
+        path.moveTo(left + position, height);
         path.lineTo(left - indicatorWidth / 2f + position, 0);
         path.lineTo(left + indicatorWidth / 2f + position, 0);
-        path.lineTo(left + position, getHeight());
+        path.lineTo(left + position, height);
         path.close();
 
         canvas.drawPath(path, indicatorPaint);
